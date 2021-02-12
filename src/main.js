5cbb8f2d2b481728da56adc61f0df6dda14b5ba2
DanielAdeyemi <adeyemidany@gmail.com>
2021-02-12 13:34:27 -0800

    add functionality to check empty input

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ function showRate(response, change) {
   if (response.result === 'success') {
     return $('.result').text((change in response.conversion_rates) ?
       (`${(response.conversion_rates[change])}`) :
-      (`There was an error: This type of currency not available right now.`));
+      (`There was an error: This type of currency not available right now or you try to input invalid symbol.`));
   } else {
     $('.error').text(`There was an error: ${Object.values(response)[1]}`);
   }
@@ -17,6 +17,8 @@ function showRate(response, change) {
 $('#convert').click(function(event) {
   event.preventDefault();
   const change = $('#change').val().toUpperCase();
+  if (!change)
+    return $('.result').text(`No currency to convert, please, try again`);
   Converter.convert()
     .then(function(response) {
       showRate(response, change);
