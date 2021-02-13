240a8921c5debdc7e75e2a83baf653e32278afca
DanielAdeyemi <adeyemidany@gmail.com>
2021-02-12 16:36:20 -0800

    make calculator works with user's input

--- src/main.js
+++ src/main.js
@@ -4,10 +4,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Converter from './js/business_logic.js';
 
-function showRate(response, change) {
+function showRate(response, change, number) {
   if (response.result === 'success' && change in response.conversion_rates) {
     $('.output').show();
-    return $('.result').text(`${(response.conversion_rates[change])}`);
+    return $('.result').text(`${(response.conversion_rates[change] * number)}`);
   } else {
     $('.error').show();
     return (response.result !== 'success') ? $('#errorResult').text(`There was an error: ${Object.values(response)[1]}`) :
@@ -15,33 +15,47 @@ function showRate(response, change) {
   }
 }
 
-$('.btn-warning').click(function(event1) {
+$('form').submit(function(event1) {
   event1.preventDefault();
-  $('#currencyChoice, .btn-warning').hide();
-  let change = $('#choices').val();
-  $('#convert').show();
-  // const change = $('#change').val().toUpperCase();
-  if (change === 'Other') {
-    $('#other').show();
-    change = $('#other').val().toUpperCase();
-  }
-  $('#convert').click(function(event2) {
-    event2.preventDefault();
-    if (!change) {
-      $('.error').show();
-      return $('#errorResult').text(`No currency to convert. Please, try again`);
-    }
-    Converter.convert()
-      .then(function(response) {
-        showRate(response, change);
-      });
-  });
+  let input = $('input#symbol').val().toUpperCase();
+  let number = $('input#num').val();
+  Converter.convert()
+    .then(function(response) {
+      showRate(response, input, number);
+    });
 });
 
-$('.again').click(function(event1) {
-  event1.preventDefault();
-  $('p').text('');
-  $('#main')[0].reset();
-  $('.output, .error, #convert').hide();
-  $('#main, .btn-warning').show();
+$('.again').click(function(event2) {
+  event2.preventDefault();
+  // $('input').text('');
+  $('form')[0].reset();
+  $('.output, .error, #convert, #other').hide();
+  $('#currencyChoice, .btn-success').show();
 });
+
+// $('.btn-success').click(function(event1) {
+//   event1.preventDefault();
+//   $('#currencyChoice, .btn-warning').hide();
+//   let change;
+//   $('#convert').show();
+//   // const change = $('#change').val().toUpperCase();
+//   // if ($('#choices').val() === 'Other') {
+//   //   $('#other').show();
+//   //   change = $('#other').val().toUpperCase();
+//   // } else {
+//   change = $('#choices').val();
+//   // }
+//   // $('#convert').click(function(event2) {
+//   //   event2.preventDefault();
+//   //   if (!change) {
+//   //     $('.error').show();
+//   //     $('p').text('');
+//   //     return $('#errorResult').text(`No currency to convert. Please, try again`);
+//   //   } else {
+//   Converter.convert()
+//     .then(function(response) {
+//       showRate(response, change);
+//     });
+// });
+// //   });
+// // });
