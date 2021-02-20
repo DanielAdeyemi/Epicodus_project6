59356099c365dd5bd8b4518afede33b4dcd1c9d1
Daniel Adeyemi <62914364+DanielAdeyemi@users.noreply.github.com>
2021-02-19 16:57:58 -0800

    Delete __tests__ directory

--- __tests__/business_logic.test.js
+++
@@ -1,9 +0,0 @@
-// import Age from '../src/js/calculator.js';
-
-// describe('Age', () => {
-//   test('should correctly create an Age object with age and expectancy for male', () => {
-//     const age = new Age(34, 'male');
-//     expect(age.years).toEqual(34);
-//     expect(age.expect).toEqual(42);
-//   });
-// });
